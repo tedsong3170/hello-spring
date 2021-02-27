f5a2928edcd5dc18e6b5124f3af0bf54f9debb24
SONG <song@SONGui-MacBookPro.local>
2021-02-28 02:08:28 +0900

    test case 추가

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,10 +2,7 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository  implements MemberRepository
 {
@@ -21,19 +18,27 @@ public class MemoryMemberRepository  implements MemberRepository
     }
 
     @Override
-    public Optional<Member> findById(Long id) {
+    public Optional<Member> findById(Long id)
+    {
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
-    public Optional<Member> findByName(String name) {
+    public Optional<Member> findByName(String name)
+    {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
 
     @Override
-    public List<Member> findAll() {
-        return new
+    public List<Member> findAll()
+    {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clear()
+    {
+        store.clear();
     }
 }
